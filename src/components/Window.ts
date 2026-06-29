407bb7ac0b85b177a47a9b1ae63ff454722f3ae7
Yukirios <99325934+Yukirios@users.noreply.github.com>
2026-06-29 16:47:00 +0300

    refactor: added titlebar

--- src/components/Window.ts
+++ src/components/Window.ts
@@ -6,8 +6,20 @@ export function Window(): string {
             <div class="window">
 
                 <div class="titlebar">
-                    <span>MemoniaOS v0.0.2</span>
-                </div>
+
+    <span>MemoniaOS v0.0.3</span>
+
+    <div class="window-buttons">
+
+        <span>─</span>
+
+        <span>□</span>
+
+        <span>✕</span>
+
+    </div>
+
+</div>
 
                 <div class="content">
 

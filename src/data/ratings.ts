96e602eb9acfc6fa308728e7a9ce66438611729d
Yukirios <99325934+Yukirios@users.noreply.github.com>
2026-06-29 17:07:14 +0300

    refactor: ratings are now a separate component

---
+++ src/data/ratings.ts
@@ -0,0 +1,75 @@
+import type { Rating } from "../types/rating.ts"
+
+export const ratings: Rating[] = [
+
+    {
+
+        id:1,
+
+        key:"1",
+
+        icon:"🗑️",
+
+        title:"В корзине что-то появилось...",
+
+        description:"Профессионально удаляем блядь"
+
+    },
+
+    {
+
+        id:2,
+
+        key:"2",
+
+        icon:"🤧",
+
+        title:"Носяра шмыгает",
+
+        description:"Воняет, но терпимо..."
+
+    },
+
+    {
+
+        id:3,
+
+        key:"3",
+
+        icon:"✝️",
+
+        title:"Прости, Господи...",
+
+        description:"Терпимо, но ужасно..."
+
+    },
+
+    {
+
+        id:4,
+
+        key:"4",
+
+        icon:"🩳",
+
+        title:"Наши штаны сухи, капитан",
+
+        description:"Продолжаем поиск."
+
+    },
+
+    {
+
+        id:5,
+
+        key:"5",
+
+        icon:"👑",
+
+        title:"Уфф... уфф... уфф...",
+
+        description:"Кандидат и точка."
+
+    }
+
+]
---
+++ src/types/Rating.ts
@@ -0,0 +1,13 @@
+export interface Rating {
+
+    id:number
+
+    key:string
+
+    icon:string
+
+    title:string
+
+    description:string
+
+}

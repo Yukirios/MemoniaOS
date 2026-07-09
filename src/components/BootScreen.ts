b58c8530b71f04815943b3ce88a756407bd311e1
Yukirios <99325934+Yukirios@users.noreply.github.com>
2026-07-09 16:20:40 +0300

    feat: added bootfiles for bootscreen component

---
+++ src/components/BootScreen.ts
@@ -0,0 +1,35 @@
+export function BootScreen(): string {
+
+    return `
+
+        <div class="boot">
+
+            <div class="boot-title">
+
+                MemoniaOS
+
+            </div>
+
+            <div class="boot-version">
+
+                v0.1.0 RC
+
+            </div>
+
+            <div class="boot-status">
+
+                Чем это так пахнет...
+
+            </div>
+
+            <div class="boot-bar">
+
+                <div id="boot-progress"></div>
+
+            </div>
+
+        </div>
+
+    `
+
+}
